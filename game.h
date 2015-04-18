19f96fb38c5b0e2682716a879864fb042c9c3c71
Heavy <hd2319@gmail.com>
2015-04-17 23:50:26 -0700

    Adding structures representing our in-game objects.

--- game.h
+++ game.h
@@ -1,10 +1,44 @@
 #include <SDL2/sdl.h>
+#include <glm/glm.hpp>
 #ifndef GAME_H
 #define GAME_H
 
+
+enum EnemyType { GOON, GOBLIN, ASSHOLE };
+enum TowerType { SHOOTER, SHOTTER, FLOCKA };
+enum AmmoType { SHOT, SLOW, FIRE, ROCKET };
+
 typedef struct
 {
-    
+    glm::vec3 pos;
+    glm::vec3 dir;
+    glm::vec3 vel;
+} Phys;
+
+typedef struct
+{
+    unsigned int enemyType;
+    unsigned int curHealth;
+    Phys physics;
+} Enemy;
+
+typedef struct
+{
+    unsigned int ammoType;  //Indicates ammo type, AKA what kind of bullet to put into the world.
+    Phys physics;
+} Bullet;
+
+typedef struct
+{
+    unsigned int id;
+    unsigned int type; //Type of tower, AKA what kind of tower to put into the world
+    AmmoType ammo;     //Ammo type this tower shoots.
+    Enemy* target;     //Enemy this tower targets, careful as it can be NULL
+    Phys physics;
+} Tower;
+
+typedef struct
+{
     unsigned int objectCount;
     unsigned int towerCount;
     void* towerPtrs;
